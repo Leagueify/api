a9d46fef93e517c1d56e521e5f83bdfbcc080723
Michael duBois <mdubois@bytement.com>
2024-04-17 13:53:27 -0600

    Add Util CalculateAge Tests

--- internal/util/util.go
+++ internal/util/util.go
@@ -4,15 +4,19 @@ import (
 	"time"
 )
 
+// TODO: Add Error if Age is < 0
 func CalculateAge(submitted, comparison string) (int, error) {
 	submittedDate, err := time.Parse(time.DateOnly, submitted)
 	if err != nil {
 		return 0, err
 	}
-	currentDate := time.Now()
-	yearsDiff := currentDate.Year() - submittedDate.Year()
-	if currentDate.Month() < submittedDate.Month() ||
-		(currentDate.Month() == submittedDate.Month() && currentDate.Day() < submittedDate.Day()) {
+	comparisonDate, err := time.Parse(time.DateOnly, comparison)
+	if err != nil {
+		return 0, err
+	}
+	yearsDiff := comparisonDate.Year() - submittedDate.Year()
+	if comparisonDate.Month() < submittedDate.Month() ||
+		(comparisonDate.Month() == submittedDate.Month() && comparisonDate.Day() < submittedDate.Day()) {
 		yearsDiff--
 	}
 	return yearsDiff, nil
